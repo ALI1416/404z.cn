465e6b1885dbbf5044592a6da219811ce0072d94
ALI <1416978277@qq.com>
2023-06-09 11:10:32 +0800

    0609

--- script/publish.js
+++ script/publish.js
@@ -6,5 +6,5 @@ fs.cpSync('./README.md', './dist/README.md');
 fs.cpSync('./css', './dist/css', {recursive: true});
 fs.cpSync('./js', './dist/js', {recursive: true});
 
-fs.cpSync('./github-contribution-grid-snake.svg', './images/github-contribution-grid-snake.svg');
-fs.cpSync('./github-contribution-grid-snake-dark.svg', './images/github-contribution-grid-snake-dark.svg');
+fs.cpSync('./github-contribution-grid-snake.svg', './dist/images/github-contribution-grid-snake.svg');
+fs.cpSync('./github-contribution-grid-snake-dark.svg', './dist/images/github-contribution-grid-snake-dark.svg');
