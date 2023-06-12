f63be2ac6637ed37c4f47b5411abeaeef4e59c3e
ALI <1416978277@qq.com>
2023-06-12 15:47:31 +0800

    0612

--- script/files.js
+++ script/files.js
@@ -2,10 +2,10 @@ const https = require('https');
 const fs = require('fs');
 
 fs.mkdirSync('./dist/files/ip2region/v3.0.0/data/', {recursive: true})
-saveFile('https://github.com/ALI1416/ip2region/raw/v3.0.0/data/ip2region.zdb', './dist/files/ip2region/v3.0.0/data/ip2region.zdb')
+saveFile('https://raw.githubusercontent.com/ALI1416/ip2region/v3.0.0/data/ip2region.zdb', './dist/files/ip2region/v3.0.0/data/ip2region.zdb')
 fs.mkdirSync('./dist/files/phone2region/v2.0.0/data/', {recursive: true})
-saveFile('https://github.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.zdb', './dist/files/phone2region/v2.0.0/data/phone2region.zdb')
-saveFile('https://github.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.txt', './dist/files/phone2region/v2.0.0/data/phone2region.txt')
+saveFile('https://raw.githubusercontent.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.zdb', './dist/files/phone2region/v2.0.0/data/phone2region.zdb')
+saveFile('https://raw.githubusercontent.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.txt', './dist/files/phone2region/v2.0.0/data/phone2region.txt')
 
 /**
  * 保存文件
