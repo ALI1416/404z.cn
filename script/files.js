135c5862a17443e23b0da9688627abe08e0653cc
ALI <1416978277@qq.com>
2023-06-12 15:40:09 +0800

    0612

--- script/files.js
+++ script/files.js
@@ -1,9 +1,9 @@
 const https = require('https');
 const fs = require('fs');
 
-fs.mkdirSync('./dist/files/ip2region/v3.0.0/', {recursive: true})
+fs.mkdirSync('./dist/files/ip2region/v3.0.0/data/', {recursive: true})
 saveFile('https://github.com/ALI1416/ip2region/raw/v3.0.0/data/ip2region.zdb', './dist/files/ip2region/v3.0.0/data/ip2region.zdb')
-fs.mkdirSync('./dist/files/phone2region/v2.0.0/', {recursive: true})
+fs.mkdirSync('./dist/files/phone2region/v2.0.0/data/', {recursive: true})
 saveFile('https://github.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.zdb', './dist/files/phone2region/v2.0.0/data/phone2region.zdb')
 saveFile('https://github.com/ALI1416/phone2region/raw/v2.0.0/data/phone2region.txt', './dist/files/phone2region/v2.0.0/data/phone2region.txt')
 
