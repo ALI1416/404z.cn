1818e85d1719d01716c901bd8f1af1dc3fa1f617
ALI <1416978277@qq.com>
2024-08-17 11:38:53 +0800

    0817

--- api/ip/index.ts
+++ api/ip/index.ts
@@ -2,7 +2,7 @@ import {VercelRequest, VercelResponse} from '@vercel/node'
 import {contentType, contentTypeJson} from '../constants'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-  let ip = (request.headers['x-real-ip'] as string).substring(7)
+  let ip = request.headers['x-real-ip']
   response.status(200)
     .setHeader(contentType, contentTypeJson)
     .send({ip})
