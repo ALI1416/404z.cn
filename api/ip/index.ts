8a38f23a8ea7051594373684d1e05135aac13e09
ALI <1416978277@qq.com>
2026-08-22 10:51:16 +0800

    0822

--- api/ip/index.ts
+++ api/ip/index.ts
@@ -1,9 +1,10 @@
 import {VercelRequest, VercelResponse} from '@vercel/node'
-import {contentType, contentTypeJson} from '../../src/Constant'
+import {cacheControl, cacheControlNo, contentType, contentTypeJson} from '../../src/Constant'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   let ip: string | string[] | undefined = request.headers['x-real-ip']
   response.status(200)
-    .setHeader(contentType, contentTypeJson)
-    .send({ip})
+  .setHeader(contentType, contentTypeJson)
+  .setHeader(cacheControl, cacheControlNo)
+  .send({ip})
 }
--- src/Constant.ts
+++ src/Constant.ts
@@ -5,11 +5,12 @@ const contentTypeSvg: string = 'image/svg+xml; charset=utf-8'
 
 const cacheControl: string = 'Cache-Control'
 const cacheControlMaxAge: string = 'max-age=21600, s-maxage=43200, stale-while-revalidate=86400'
+const cacheControlNo: string = 'private, no‑store, no‑cache, must‑revalidate, max‑age=0'
 
 const textNotFound: string = '404 NOT FOUND'
 
 export {
   contentType, contentTypePlain, contentTypeJson, contentTypeSvg,
-  cacheControl, cacheControlMaxAge,
+  cacheControl, cacheControlMaxAge, cacheControlNo,
   textNotFound,
 }
