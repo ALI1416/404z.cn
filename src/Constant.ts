fd75d1457812682d25d96d02b255a3ab996a3157
ALI <1416978277@qq.com>
2026-08-22 11:04:22 +0800

    0822

--- src/Constant.ts
+++ src/Constant.ts
@@ -5,7 +5,7 @@ const contentTypeSvg: string = 'image/svg+xml; charset=utf-8'
 
 const cacheControl: string = 'Cache-Control'
 const cacheControlMaxAge: string = 'max-age=21600, s-maxage=43200, stale-while-revalidate=86400'
-const cacheControlNo: string = 'private, no‑store, no‑cache, must‑revalidate, max-age=0'
+const cacheControlNo: string = 'private, no-store, no-cache, must-revalidate, max-age=0'
 
 const textNotFound: string = '404 NOT FOUND'
 
